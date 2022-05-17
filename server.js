83da404f556559f210e4ef13004264e7cb6bd0ef
Beepo <micahstew@gmail.com>
2022-05-17 16:06:17 -0400

    updating server.js

--- server.js
+++ server.js
@@ -47,11 +47,11 @@ function deleteNote(id, notesArray) {
   for (let i = 0; i < notesArray.length; i++) {
       let note = notesArray[i];
 
-      if (note.id == id) {
+     if (note.id == id) {
           notesArray.splice(i, 1);
           fs.writeFileSync(
-              path.join(__dirname, './db/db.json'),
-              JSON.stringify(notesArray, null, 2)
+             path.join(__dirname, './db/db.json'),
+             JSON.stringify(notesArray, null, 2)
           );
 
           break;
